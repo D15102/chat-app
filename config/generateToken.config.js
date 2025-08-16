98b8564676c2e208129f7430b7c147fe5f144459
d15102 <surtidharmik05@gmail.com>
2025-08-16 22:39:18 +0530

    First Commit with Basic Protected Routes

--- config/generateToken.config.js
+++ config/generateToken.config.js
@@ -4,6 +4,7 @@ export const generateToken = (req, res, userData) => {
     const token = jwt.sign({ userId: userData._id }, process.env.JWT_SECRET, { expiresIn: "1h" })
 
     res.cookie('token', token, {
+        httpOnly: true,
         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         secure: process.env.NODE_ENV === 'production',
         domain: process.env.NODE_ENV === 'production' ? '.onrender.com' : undefined
