52ce42af2ccfcec1292c8dcf3e1a6b3adba74df4
d15102 <surtidharmik05@gmail.com>
2025-08-16 23:04:17 +0530

    First Commit with Basic Protected Routes

--- config/generateToken.config.js
+++ config/generateToken.config.js
@@ -8,6 +8,7 @@ export const generateToken = (req, res, userData) => {
         secure: process.env.NODE_ENV === "production",
         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 60 * 60 * 1000, // 1 hour in milliseconds
+        
     }
 
     // Add domain for production (Render deployment)
