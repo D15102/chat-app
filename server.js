05df71d70a1a2e614de5ff26c9816ac093244743
d15102 <surtidharmik05@gmail.com>
2025-08-16 23:52:01 +0530

    First Commit with Basic Protected Routes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express()
 const PORT = process.env.PORT
 
 app.use(cors({
-    origin: process.env.CLIENT_URL,
+    origin: [process.env.CLIENT_URL,'https://chat-app-client-ashy-two.vercel.app/'],
     credentials: true
 }))
 app.use(express.json())
