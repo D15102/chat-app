8ea6b885a6ad7ad0558b472a73ab2632d6551f72
d15102 <surtidharmik05@gmail.com>
2025-08-16 19:40:16 +0530

    Second Commit

--- config/generateToken.config.js
+++ config/generateToken.config.js
@@ -5,7 +5,7 @@ export const generateToken = (req, res, userData) => {
     res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax"
+        sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax"
     })
 
 }
