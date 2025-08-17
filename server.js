0d994959bfdd82d68ecc0a2cd27eee9e90b79516
d15102 <surtidharmik05@gmail.com>
2025-08-17 09:46:29 +0530

    Commit With Some Production Ready Changes

--- server.js
+++ server.js
@@ -3,19 +3,28 @@ import { config } from 'dotenv'
 import { connectDB } from './config/mongodb.config.js'
 import userRoutes from './routes/userRoutes.js'
 import cors from 'cors'
+import path from 'path'
 config()
 import cookieParser from 'cookie-parser'
 const app = express()
 const PORT = process.env.PORT
 
 app.use(cors({
-    origin: [process.env.CLIENT_URL,'https://chat-app-client-ashy-two.vercel.app/'],
+    origin: process.env.CLIENT_URL,
     credentials: true
 }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use('/users', userRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    const dirPath = path.resolve()
+    app.use(express.static("./frontend/dist"))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(dirPath, "./frontend/dist", "index.html"))
+    })
+}
 
 app.listen(PORT, () => {
     connectDB()
