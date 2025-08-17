481cf99677fc759ab15b537d0603290403a2beff
d15102 <surtidharmik05@gmail.com>
2025-08-17 11:26:42 +0530

    5th Commit

--- config/generateToken.config.js
+++ config/generateToken.config.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken'
 
 export const generateToken = (req, res, userData) => {
+    
     const token = jwt.sign({ userId: userData._id }, process.env.JWT_SECRET, { expiresIn: "1h" })
     res.cookie('token', token, {
         httpOnly: true,
